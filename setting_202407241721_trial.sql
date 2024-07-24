8c080841f442ebb97e998ca76c08365fbbdb8d74
ristian.ekahendi@duakelinci.co.id <ristian.ekahendi@duakelinci.co.id>
2024-07-24 17:22:19 +0700

    commit database

---
+++ setting_202407241721_trial.sql
@@ -0,0 +1,2 @@
+INSERT INTO test_db.setting (instansi,alamat_instansi,kabupaten,profinsi,nama_aplikasi,logo,background,icon,email_notif,password_email) VALUES
+	 ('BASIC YII2','Surabaya','Surabaya','Jawa TImur','BASIC YII','/img/lotus.webp',NULL,'/img/favicon.ico',NULL,NULL);
